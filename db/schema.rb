792ee7702c87f3d5c5b7a79f3dadd2ea271e4364
cree <andersan@umich.edu>
2017-02-21 13:16:38 -0300

    fix merge conflict

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160505140008) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "monologue_posts", force: :cascade do |t|
     t.boolean  "published"
     t.datetime "created_at"
@@ -24,21 +27,21 @@ ActiveRecord::Schema.define(version: 20160505140008) do
     t.datetime "published_at"
   end
 
-  add_index "monologue_posts", ["url"], name: "index_monologue_posts_on_url", unique: true
+  add_index "monologue_posts", ["url"], name: "index_monologue_posts_on_url", unique: true, using: :btree
 
   create_table "monologue_taggings", force: :cascade do |t|
     t.integer "post_id"
     t.integer "tag_id"
   end
 
-  add_index "monologue_taggings", ["post_id"], name: "index_monologue_taggings_on_post_id"
-  add_index "monologue_taggings", ["tag_id"], name: "index_monologue_taggings_on_tag_id"
+  add_index "monologue_taggings", ["post_id"], name: "index_monologue_taggings_on_post_id", using: :btree
+  add_index "monologue_taggings", ["tag_id"], name: "index_monologue_taggings_on_tag_id", using: :btree
 
   create_table "monologue_tags", force: :cascade do |t|
     t.string "name"
   end
 
-  add_index "monologue_tags", ["name"], name: "index_monologue_tags_on_name"
+  add_index "monologue_tags", ["name"], name: "index_monologue_tags_on_name", using: :btree
 
   create_table "monologue_users", force: :cascade do |t|
     t.string   "name"
