6bdd921bccef0e9649491193ad431f6f33734703
cree <andersan@umich.edu>
2017-02-23 20:57:47 -0300

    updated to fancybox 2, changed content of main page

--- Gemfile
+++ Gemfile
@@ -22,6 +22,8 @@ gem 'rake', '~> 0.9.6'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
+# Add fancybox JS plugin for images
+gem 'fancybox2-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
 gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,3 +7,11 @@ Rails.application.config.assets.version = '1.0'
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += %w( fancybox/source/jquery.fancybox.css 
+	fancybox/source/jquery.fancybox.pack.js 
+	fancybox/source/helpers/jquery.fancybox-buttons.css
+	fancybox/source/helpers/jquery.fancybox-buttons.js
+	fancybox/source/helpers/jquery.fancybox-media.js
+	fancybox/source/helpers/jquery.fancybox-thumbs.css
+	fancybox/source/helpers/jquery.fancybox-thumbs.js
+	fancybox/lib/jquery.mousewheel-3.0.6.pack.js)
